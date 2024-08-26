b05bca3bfa4905ee5c4776c4480f161017015125
Eduardo Frederico <eduardo.frederico@icloud.com>
2024-08-26 20:44:51 -0300

    Initial Commit

---
+++ MovieQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieQuiz
+//
+//  Created by Eduardo Frederico on 26/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
